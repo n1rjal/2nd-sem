b7fc6f2ad781bbe1f6e17b2c5d2fb1dd0c81b825
n1rjal <nirjalpaudel54312@gmail.com>
2021-02-28 15:05:51 +0545

    Added virtual fix

---
+++ Sun FEB 28/Virtual fix/error.cpp
@@ -0,0 +1,49 @@
+#include<iostream> 
+using namespace std; 
+
+
+// Analyse this program and use " Virtual " Keyword to avoid diamond problem.
+class Person
+{
+    //data members of person
+public:
+    Person(int x)
+    {
+        cout << "Person" << x << "Called" << endl;
+    }
+    Person()
+    {
+        cout << "Person default constructor called" <<endl;
+    }
+};
+class Faculty : virtual public Person
+{
+    //data members of faculty
+public:
+    Faculty(int x) : Person(x)
+    {
+        cout << "Faculty" << x << "Called" << endl;
+    }
+};
+class Student : virtual public Person
+{
+    //data member of faculty
+public:
+    Student(int x) : Person(x)
+    {
+        cout << "Student" << x << "Called" << endl;
+    }
+};
+class TA : public Faculty, public Student
+{
+public:
+    TA(int x) : Faculty(x), Student(x)
+    {
+        cout << "TA" << x << "Called" << endl;
+    }
+};
+int main()
+{
+    TA ta1(30);
+    return 0;
+}
