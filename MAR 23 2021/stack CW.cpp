7e22d1fa11049fe2fd91736c443012952217a982
n1rjal <nirjalpaudel54312@gmail.com>
2021-03-23 12:09:31 +0545

    added postfix

--- MAR 23 2021/stack CW.cpp
+++ MAR 23 2021/stack CW.cpp
@@ -7,18 +7,23 @@ class Stack{
         Type value;
     public:
         Stack(Type t1){
-            this->value = ++t1;
-        }
+            this->value = t1;
+        };
         Type display_value(){
             return this->value;
-        }
+        };
+        Type operator ++(int){
+            return this->value++;
+        };
 };
 
 int main(){
     Stack<int> s1(44);
     Stack<double> s2(451212);
     Stack<float> s3(44.44);
-
+    s1++;
+    s2++;
+    s3++;
     cout << s1.display_value()<<endl;
     cout << s2.display_value()<<endl;
     cout << s3.display_value()<<endl;
